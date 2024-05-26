59449c3ca138293014a31f6d2f6a54b65afa8eb2
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-05-26 17:20:27 +0700

    add google analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://karuhun.dev/',
+    integrations: [
+        partytown({
+            config: {
+                forward: ["dataLayer.push"]
+            }
+        }),
+        sitemap(),
+    ]
+});
