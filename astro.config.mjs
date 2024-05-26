c9282680ea605181df55c34851e320b918783f68
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-05-26 17:42:54 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
+import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://karuhun.dev/',
+    output: 'static',
+    adapter: vercelStatic(),
     integrations: [
         partytown({
             config: {
