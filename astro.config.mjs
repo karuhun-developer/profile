cca14be9d794755ee49d5d22edbbe5e52c7f143a
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-06-01 17:21:09 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://karuhun.dev/',
+    site: 'https://karuhundeveloper.com/',
     output: 'static',
     adapter: vercelStatic(),
     integrations: [
